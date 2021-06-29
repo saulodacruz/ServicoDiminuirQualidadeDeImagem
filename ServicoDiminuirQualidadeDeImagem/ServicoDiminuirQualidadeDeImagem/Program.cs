893f1fda3df18f590be7ac718388b207ce88a507
Saulo Cruz <saulo.cruz@smanuvemlive.onmicrosoft.com>
2021-06-29 16:14:31 -0300

    ok

---
+++ ServicoDiminuirQualidadeDeImagem/ServicoDiminuirQualidadeDeImagem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ServicoDiminuirQualidadeDeImagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o caminho do arquivo origem: ");
+            var pathOrigin = Console.ReadLine();
+
+            Console.WriteLine("Digite o novo caminho do arquivo: ");
+            var newPath = Console.ReadLine();
+
+            Console.WriteLine("Digite o percentual para diminuir a imagem: ");
+            var percentage = int.Parse(Console.ReadLine());
+
+            var servico = new Service(pathOrigin, newPath, percentage);
+            try
+            {
+                servico.Run();
+                Console.WriteLine("Imagem reduzida com sucesso !");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Houve um problema: " + ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ServicoDiminuirQualidadeDeImagem/ServicoDiminuirQualidadeDeImagem/Service.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+
+namespace ServicoDiminuirQualidadeDeImagem
+{
+    public class Service
+    {
+        readonly string _sourceFile;
+        readonly string _destinyTreatedFile;
+        readonly int _percentageQuality;
+
+        public Service(string sourceFile, string destinyTreatedFile, int percentageQuality)
+        {
+            _sourceFile = sourceFile;
+            _destinyTreatedFile = destinyTreatedFile;
+            _percentageQuality = percentageQuality;
+        }
+
+        public void Run()
+        {
+            var img = Image.FromFile(_sourceFile);
+            CompressImage(img, _percentageQuality, _destinyTreatedFile);
+        }
+
+        private void CompressImage(Image imagem, long qualidade, string file)
+        {
+            var param = new EncoderParameters(1);
+            param.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, qualidade);
+            var codec = ObterCodec(imagem.RawFormat);
+            imagem.Save(file, codec, param);
+        }
+
+        private ImageCodecInfo ObterCodec(ImageFormat formato)
+        {
+            var codec = ImageCodecInfo.GetImageDecoders().FirstOrDefault(c => c.FormatID == formato.Guid);
+            if (codec == null) throw new NotSupportedException();
+            return codec;
+        }
+    }
+}
